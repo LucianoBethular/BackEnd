ca4249d2fc1d0687a87c3b09a61fe7849857c1b0
LucianoBethular <77418936+LucianoBethular@users.noreply.github.com>
2021-08-31 20:23:08 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
+const handlebars = require("express-handlebars")
+const express = require("express");
 
-import express from 'express';
-import multer from 'multer';
 
 
 const app = express();
-const router = express.Router();
+//const router = express.Router();
 const PORT = 8080;
 
-const productos = []
 
 
-const server = app.listen(PORT, () =>{
-console.log(`servidor escuchando en puerto ${server.address().port}`)}
-)
-server.on("error",error => console.log(`Error en servidor ${error}`));
+const server = app.listen(PORT, () => {
+console.log(`servidor escuchando en puerto ${server.address().port}`);
+});
+server.on("error", (error) => console.log(`Error en servidor ${error}`));
 
-
-app.use('/api', import('./rutas/productos.route.js'));
+app.use("/", require("./rutas/productos.route"));
 app.use(express.json());
-app.use(express.static('public'))
+app.engine("hbs", handlebars({
+    extname:".hbs",
+    defaultLayout:"main.hbs",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+}
+))
+app.set("view engine", "hbs");
+app.set("views", "./views")
+app.use(express.static(__dirname + 'public'))
+app.use(express.urlencoded())
+
 
 
 
