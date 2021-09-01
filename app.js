a3ca5e7e7b86719a9409a8901aff8167a9116ed9
LucianoBethular <77418936+LucianoBethular@users.noreply.github.com>
2021-09-01 14:52:21 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,76 +1,27 @@
-const handlebars = require("express-handlebars")
+
 const express = require("express");
 
 
-//import multer from "multer";
-const router = require("./rutas/productos.route");
+
 const app = express();
 //const router = express.Router();
 const PORT = 8080;
 
-const productos = [];
 
-router.use(function timeLog(req, res, next) {
-    console.log('Time: ', Date.now());
-    next();
-});
-app.use(express.json());
+
 const server = app.listen(PORT, () => {
 console.log(`servidor escuchando en puerto ${server.address().port}`);
 });
 server.on("error", (error) => console.log(`Error en servidor ${error}`));
-app.use("/api", require("./rutas/productos.route"));
+
+app.use("/", require("./rutas/productos.route"));
 app.use(express.json());
 
-
-
-app.engine("hbs", handlebars({
-
-    extname:".hbs",
-    defaultLayout:"main.hbs",
-    layoutsDir: __dirname + "/views/layouts",
-    partialsDir: __dirname + "/views/partials",
-
-
-}
-))
-
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "./views")
 app.use(express.static(__dirname + 'public'))
 app.use(express.urlencoded())
 
-app.get("/api/productos/vistas", (req,res )=>{
-    res.render("main", {productos})
-})
-
-app.get("/", (req,res )=>{
-    res.render("index", {titulo: "Pagina principal"})
-})
-
-app.get("/api", (req,res )=>{
-    res.render("index", {titulo: "Pagina principal"})
-})
-
-app.get("/api/productos/guardar",  (req, res) => {
-    res.render("index",{productos, titulo:"Complete los campos para agregar objeto"} )
-
-}); 
 
 
-app.post("/api/productos/guardar", (req, res) => {
-//    let name = req.body.producto;
-//    let price = req.body.precio;
-//    let thumbnail = req.body.foto;
-//    const producto = {
-//    name: name,
-//    price: price,
-//    thumbnail: thumbnail,
-//    };
-    productos.push({ ...req.body,  id: productos.length });
-res.render("index", {productos, titulo:"Complete los campos para agregar objeto"})
-// console.log(productos)
-//    res.json(productos)
-
-});
 
--- rutas/productos.route.js
+++ rutas/productos.route.js
@@ -1,16 +1,32 @@
+
 const express = require("express");
-//const app = express();
 
 const router = express.Router();
-const productos = [];
+
+
+
+
+
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now());
     next();
 });
+
+
+const productos = [];
+
+router.use(express.urlencoded({extended:true}))
+router.use(express.json());
+router.get("/api", (req,res )=>{
+    res.render("index", {productos , titulo: "Pagina principal"})
+})
+
+
+
 router.get("/", (req,res )=>{
-  res.render("index", console.log("HOLA"))
+  res.render("index", {productos, titulo: "Pagina principal"})
 })
 
 router.get("/api/productos/vistas", (req,res )=>{
@@ -20,6 +36,7 @@ router.get("/api/productos/listar", (req, res) => {
 if (productos.length == 0) res.send("error no hay productos cargados");
 else res.json({ items: productos, cantidad: productos.length });
 });
+
 router.get("/api/productos/listar/:id", (req, res) => {
 if (!isNaN(req.params.id)) {
     let id = parseInt(req.params.id, 10);
@@ -28,33 +45,51 @@ if (!isNaN(req.params.id)) {
     else res.send("producto no existe");
   } else res.send("esto no es un numero ");
 });
+
 router.put("/api/productos/actualizar/:id", (req, res) => {
   if (!isNaN(req.params.id)) {
     let id = parseInt(req.params.id, 10);
     if (id > 0 && id <= productos.length)
-      res.json(productos[req.params.id - 1]);
+      res.json(productos[req.params.id - 1]+"ha sido actualizado");
     else res.send("producto no existe");
   } else res.send("esto no es un numero ");
 });
+
+
+
 router.delete("/api/productos/borrar/:id", (req, res) => {
+  
   if (!isNaN(req.params.id)) {
     let id = parseInt(req.params.id, 10);
     if (id > 0 && id <= productos.length)
-    res.json(productos[req.params.id - 1]);
+      res.json(productos[req.params.id - 1]+"ha sido borrado");
     else res.send("producto no existe");
-} else res.send("esto no es un numero ");
+  } else res.send("esto no es un numero ");
 });
-router.post("/api/productos/guardar", (req, res) => {
-  let name = req.body.producto;
-  let price = req.body.precio;
-  let thumbnail = req.body.foto;
-  const producto = {
-    name: name,
-    price: price,
-    thumbnail: thumbnail,
-  };
-  productos.push({ ...producto, id: productos.length });
-  res.json(producto);
+
+
+router.get("/api/productos/guardar",  (req, res) => {
+  res.render("index", {productos, titulo:"Complete los campos para agregar objeto"} )
+
+}); 
+
+router.post("/api/productos/guardar-datos", (req, res) => {
+//    let name = req.body.producto;
+//    let price = req.body.precio;
+//    let thumbnail = req.body.foto;
+//    const producto = {
+//    name: name,
+//    price: price,
+//    thumbnail: thumbnail,
+//    };
+  productos.push({ ...req.body,  id: productos.length });
+res.render("index", {productos, titulo:"Complete los campos para agregar objeto"})
+// console.log(productos)
+  //res.json(productos)
+
 });
+
+
+
 
 module.exports = router;
